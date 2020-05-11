7744039497efc879cff8d0f24a007389d7c7f0c6
Ahmed Rizwan <ahmed@snappymob.com>
2020-05-11 18:21:55 +0500

    Initial commit

---
+++ app/src/main/java/com/sudo/rizwan/composecalculator/Theme.kt
@@ -0,0 +1,13 @@
+package com.sudo.rizwan.composecalculator
+
+import androidx.ui.graphics.Color
+import androidx.ui.material.darkColorPalette
+
+val darkBackgroundColor = Color(0xFF15202b)
+val primaryColor = Color(0xFF08a0e9)
+
+val darkThemeColors = darkColorPalette(
+    primary = primaryColor,
+    background = darkBackgroundColor,
+    surface = darkBackgroundColor
+)
